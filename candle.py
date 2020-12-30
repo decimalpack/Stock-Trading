efce8c221f4e116159769dfe0b65f786cedace93
decimalpack <decimalpack@users.noreply.github.com>
2020-12-31 00:15:00 +0530

    Added Trend detection and Confirmation

--- candle.py
+++ candle.py
@@ -1,20 +1,47 @@
-import talib
-import sys
 import itertools
-import yfinance as yf
-import numpy as np
+import sys
 from collections import namedtuple
+
+import numpy as np
+import talib
+import yfinance as yf
+
+
+class Trend_EMA:
+    def __init__(self, prices, period):
+        self.version = "0.0.1"
+        self.prices = prices
+        self.period = period
+
+    def direction(self):
+        ema = talib.EMA(self.prices, self.period)
+        trend = 1 if ema[-1] < self.prices[-1] else -1
+        return trend
 
 
 class CandlePatterns:
     def __init__(self, open_, high, low, close):
-        self.version = "0.0.1"
+        self.version = "0.1.1"
         self.data = open_, high, low, close
 
     def prediction(self):
         return self.detail().type
 
     def detail(self):
+        """
+        Return the most recent candlestick pattern detected
+
+        Pattern(
+            name = "The name of candlestick pattern",
+            index = "The index of most recent pattern" 
+            type = "1 if Bullish, -1 if Bearish"
+        )
+        if no pattern detected,
+        return Pattern = namedtuple("Pattern", ["name", "type", "index"])
+
+        :return: namedtuple("Pattern", ["name", "type", "index"])
+        :rtype: namedtuple
+        """
         talib_patterns = {
             "CDL2CROWS": "Two Crows",
             "CDL3BLACKCROWS": "Three Black Crows",
@@ -86,48 +113,69 @@ class CandlePatterns:
             index = np.max(np.append(np.flatnonzero(output),
                                      -1))  # Append -1 to avoid empty array
             if index > most_recent_pattern.index:
-                most_recent_pattern = Pattern(name=pat_name,
-                                              type=output[index] // 100,
-                                              index=index)
+                pattern_type = output[index] // 100
+                if pattern_type != trend:  # Bearish pattern should form in uptrend
+                    most_recent_pattern = Pattern(name=pat_name,
+                                                  type=output[index] // 100,
+                                                  index=index)
         return most_recent_pattern
 
-if __name__ == "__main__":
 
+def get_data(tickers, period):
+    data = yf.download(tickers=" ".join(tickers),
+                       progress=sys.stdout.isatty(),
+                       period=period)
+    data = data.swaplevel(0, 1, axis=1)
+    return data
+
+
+if __name__ == "__main__":
     nifty50 = [
         'DRREDDY.NS', 'BAJAJ-AUTO.NS', 'INFY.NS', 'WIPRO.NS', 'CIPLA.NS',
         'HINDALCO.NS', 'SBIN.NS', 'TITAN.NS', 'HCLTECH.NS', 'UPL.NS',
         'ICICIBANK.NS', 'GAIL.NS', 'HEROMOTOCO.NS', 'ASIANPAINT.NS', 'ITC.NS',
         'SBILIFE.NS', 'HINDUNILVR.NS', 'RELIANCE.NS', 'EICHERMOT.NS',
         'SHREECEM.NS', 'AXISBANK.NS', 'TCS.NS', 'M&M.NS', 'SUNPHARMA.NS',
-        'TECHM.NS', 'POWERGRID.NS', 'HDFCLIFE.NS', 'DIVISLAB.NS', 'NESTLEIND.NS',
-        'LT.NS', 'GRASIM.NS', 'BPCL.NS', 'ULTRACEMCO.NS', 'TATAMOTORS.NS',
-        'NTPC.NS', 'JSWSTEEL.NS', 'BAJAJFINSV.NS', 'TATASTEEL.NS', 'HDFC.NS',
-        'BHARTIARTL.NS', 'BRITANNIA.NS', 'BAJFINANCE.NS', 'COALINDIA.NS',
-        'ADANIPORTS.NS', 'KOTAKBANK.NS', 'IOC.NS', 'MARUTI.NS', 'ONGC.NS',
-        'HDFCBANK.NS', 'INDUSINDBK.NS'
+        'TECHM.NS', 'POWERGRID.NS', 'HDFCLIFE.NS', 'DIVISLAB.NS',
+        'NESTLEIND.NS', 'LT.NS', 'GRASIM.NS', 'BPCL.NS', 'ULTRACEMCO.NS',
+        'TATAMOTORS.NS', 'NTPC.NS', 'JSWSTEEL.NS', 'BAJAJFINSV.NS',
+        'TATASTEEL.NS', 'HDFC.NS', 'BHARTIARTL.NS', 'BRITANNIA.NS',
+        'BAJFINANCE.NS', 'COALINDIA.NS', 'ADANIPORTS.NS', 'KOTAKBANK.NS',
+        'IOC.NS', 'MARUTI.NS', 'ONGC.NS', 'HDFCBANK.NS', 'INDUSINDBK.NS'
     ]
-    data = yf.download(tickers=" ".join(nifty50),progress=sys.stdout.isatty(),period="5d")
-    data = data.swaplevel(0, 1, axis=1)
+    data = get_data(nifty50, period="1mo")
     detected = []
     for equity in nifty50:
-        price_history = data[equity]
-        ohlc = price_history.Open.values, price_history.High.values, price_history.Close.values, price_history[
-            "Adj Close"].values
+        price_history = data[equity][:-1]
+        ohlc = (price_history.Open.values, price_history.High.values,
+                price_history.Low.values, price_history.Close.values)
+        close = ohlc[-1]
+        trend = Trend_EMA(close, len(close) - 1).direction()
         candlePattern = CandlePatterns(*ohlc)
         pattern = candlePattern.detail()
-        if pattern.name:
-            date = price_history.iloc[pattern.index].name.strftime("%d %b %Y")
-            detected.append({
-                "equity_name": equity,
-                "pattern_name": pattern.name,
-                "pattern_type": 'Bullish' if pattern.type == 1 else 'Bearish',
-                "detected_date": date
-            })
-    print("Version:",candlePattern.version)
+        if pattern.name and pattern.index >= len(close) - 10:
+            i = pattern.index
+            bear_confirmation = data[equity].iloc[i+1].Close <= data[
+                equity].iloc[i].Low
+            bull_confirmation = data[equity].iloc[i+1].Close >= data[
+                equity].iloc[i].High
+            date = price_history.iloc[pattern.index].name
+            if pattern.type != trend and (
+                (pattern.type == 1 and bull_confirmation) or
+                (pattern.type == -1 and bear_confirmation)):
+                pat = {
+                    "equity_name": equity,
+                    "pattern_name": pattern.name,
+                    "pattern_type":
+                    'Bullish' if pattern.type == 1 else 'Bearish',
+                    "detected_date": date.strftime("%d %b %Y")
+                }
+                detected.append(pat)
+    print("Version:", candlePattern.version)
     detected.sort(key=lambda x: x["pattern_type"])
     for pattern_type, pattern in itertools.groupby(
             detected, key=lambda x: x["pattern_type"]):
-        print("\n"+pattern_type+"\n"+"="*len(pattern_type))
+        print("\n" + pattern_type + "\n" + "=" * len(pattern_type))
         for p in pattern:
             fmt = f'{p["pattern_name"]} in {p["equity_name"][:-3]} on {p["detected_date"]}'
             print(fmt)
